8115e0e2957b98841dab0154e22c628e8752b63a
Nayan Prasad P K <nayanprasad096@gmail.com>
2022-12-25 15:53:30 +0530

    mobile touch issue fixed

--- componets/Radar/Radar.jsx
+++ componets/Radar/Radar.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import NextImage from 'next/image';
 import { loadImage } from 'canvas';
+import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
+
 
 import Logo from "../../assets/logo.svg"
 import style from "./Radar.module.scss";
@@ -19,8 +21,6 @@ class MapView {
     // set background color black
 
 
-
-
     this.map.addEventListener("mousemove", this.mouseEvent, { passive: true });
     this.map.addEventListener("mousedown", this.mouseEvent, { passive: true });
     this.map.addEventListener("mouseup", this.mouseEvent, { passive: true });
@@ -30,8 +30,7 @@ class MapView {
     this.map.addEventListener("touchmove", this.mouseEvent, { passive: true });
 
     this.map.addEventListener("wheel", this.mouseWheelEvent, { passive: false });
-    this.map.addEventListener("resize", () => this.pos = { x: this.map.width / 2, y: this.map.height / 2 },
-      { passive: true });
+    this.map.addEventListener("resize", () => this.pos = { x: this.map.width / 2, y: this.map.height / 2 }, { passive: true });
 
     requestAnimationFrame(this.drawCanvas);
   }
@@ -64,16 +63,16 @@ class MapView {
       .map((v, i) => (step * (i + this.scale)) % (canvas.width * 4))
 
     radius.forEach(this.drawCircle);
-    this.set_logo()
+    // this.set_logo()
     requestAnimationFrame(this.drawCanvas);
   };
 
   set_logo() {
-    const logo = new Image();
-    logo.src = Logo;
-    logo.width = 100;
-    logo.height = 100;
-    this.mapContext.drawImage(logo, this.pos.x-logo.width/2, this.pos.y-logo.height/2, logo.width, logo.height);
+    // const logo = new Image();
+    // logo.src = Logo;
+    // logo.width = 100;
+    // logo.height = 100;
+    // this.mapContext.drawImage(logo, this.pos.x-logo.width/2, this.pos.y-logo.height/2, logo.width, logo.height);
   }
 
   mouseWheelEvent = (event) => {
@@ -101,11 +100,19 @@ class MapView {
     this.mouse.x = event.offsetX ?? event.touches[0]?.clientX;
     this.mouse.y = event.offsetY ?? event.touches[0]?.clientY;
 
+
     if (this.mouse.button && this.scale > 0) {
+
+
+      if(isMobile && event.type !== "touchmove") {
+        this.mouse.oldX = this.mouse.x;
+        this.mouse.oldY = this.mouse.y;
+      }
+
       const x = Math.max(this.mouse.x - this.mouse.oldX + this.pos.x, 0) || 0;
       const y = Math.max(this.mouse.y - this.mouse.oldY + this.pos.y, 0) || 0;
 
-      this.pos = { x: Math.min(x, this.map.width), y: Math.min(y, this.map.height) };
+      this.pos = {x: Math.min(x, this.map.width), y: Math.min(y, this.map.height)};
     }
   };
 
@@ -142,10 +149,6 @@ class MapView {
     this.mapContext.arc(this.pos.x, this.pos.y, radius, 0, 2 * Math.PI);
     this.mapContext.stroke();
   }
-
-  init = () => {
-    console.log("init");
-  }
 }
 
 const Radar = (props) => {
@@ -163,7 +166,11 @@ const Radar = (props) => {
     setSize();
 
     const map = new MapView(canvas);
+    
   }, [])
+
+
+
 
   return (
     <div>
@@ -173,4 +180,3 @@ const Radar = (props) => {
 }
 
 export default Radar
-
